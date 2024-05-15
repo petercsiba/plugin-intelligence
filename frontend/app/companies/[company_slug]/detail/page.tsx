3351b0cd627b0260382f028683894041da35b538
Peter Csiba <petherz@gmail.com>
2024-05-15 13:17:46 -0700

    Fill in companies/[company_slug]/detail/page.tsx with blank page

--- frontend/app/companies/[company_slug]/detail/page.tsx
+++ frontend/app/companies/[company_slug]/detail/page.tsx
@@ -1,1 +1,17 @@
 // TODO(P0, ux): Implement this page
+import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+export default function About() {
+    return (
+        <Container maxWidth="lg">
+            <Box sx={{ my: 4 }}>
+                <Typography variant="h4" component="h1">
+                    COMING SOON
+                </Typography>
+            </Box>
+        </Container>
+    );
+}
