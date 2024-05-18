aa41c5879da7650353f1f679249e2275b508e6b4
Peter Csiba <petherz@gmail.com>
2024-05-17 18:37:40 -0700

    [frontend] Enlist more google subdomains, in theory it's constraint by the DB contents

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: [
+      'lh3.googleusercontent.com',
+      'lh4.googleusercontent.com',
+      'lh5.googleusercontent.com',
+      'lh6.googleusercontent.com',
+      'storage.googleapis.com',
+      'drive.google.com'
+    ],
   },
 };
 
