083d3373761bb4613f0d337442723d163e34ea7a
Peter Csiba <petherz@gmail.com>
2024-05-19 07:28:57 -0700

    comment: add the SQL query to get the data for charts

--- backend/scripts/charts/histogram_user_count.py
+++ backend/scripts/charts/histogram_user_count.py
@@ -3,6 +3,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Data
+# SELECT
+#     CASE
+#         WHEN user_count IS NULL OR (user_count BETWEEN 0 AND 10) THEN '0-10'
+#         WHEN user_count BETWEEN 11 AND 100 THEN '11-100'
+#         WHEN user_count BETWEEN 101 AND 1000 THEN '101-1000'
+#         WHEN user_count BETWEEN 1001 AND 10000 THEN '1001-10000'
+#         WHEN user_count BETWEEN 10001 AND 100000 THEN '10001-100000'
+#         WHEN user_count BETWEEN 100001 AND 1000000 THEN '100001-1000000'
+#         WHEN user_count BETWEEN 1000001 AND 10000000 THEN '1000001-10000000'
+#         WHEN user_count BETWEEN 10000001 AND 100000000 THEN '10000001-100000000'
+#         WHEN user_count BETWEEN 100000001 AND 1000000000 THEN '100000001-1000000000'
+#         ELSE '1000000001+'
+#     END AS range,
+#     COUNT(*) AS count
+# FROM google_workspace
+# WHERE p_date = '2024-05-16'
+# GROUP BY range
+# ORDER BY MIN(user_count);
+
 google_workspace_data = {
     "range": [
         "0-10", "11-100", "101-1000", "1001-10000",
